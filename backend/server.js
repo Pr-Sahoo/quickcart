7df9a12105477a733bba8d972594278e051e5f11
Pr-Sahoo <pranayaranjan4@gmail.com>
2025-01-10 18:30:58 +0530

    modify server.js backend deploy only

--- backend/server.js
+++ backend/server.js
@@ -21,12 +21,12 @@ app.get('/test', (req, res) => {
 });
 
 // serve the static files from react app in production
-if(process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "build")));
-  app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, "build", "index.html"));
-  });
-}
+// if(process.env.NODE_ENV === "production") {
+//   app.use(express.static(path.join(__dirname, "build")));
+//   app.get('*', (req, res) => {
+//     res.sendFile(path.join(__dirname, "build", "index.html"));
+//   });
+// }
 
 
 app.listen(port, () => {
